ae50151d02bf2c8f2f4f3af96ef04713692ed5b9
JC on PC <jcenmiami@gmail.com>
2017-01-10 12:42:01 -0500

    Project finished. Sends arguments in eclipse.

--- com/ezitapps/IntroEclipse.java
+++ com/ezitapps/IntroEclipse.java
@@ -1,3 +1,4 @@
+
 package com.ezitapps;
 
 public class IntroEclipse {
@@ -5,6 +6,30 @@ public class IntroEclipse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("This is the Intro Eclipse Assignment for class CEN 3040");
+		
+		String fName = "";
+		String lName = "";
+		
+		if (args == null || args.length < 2) {
+			System.out.println("We need to arguments to make this work.");
+		} else {
+			fName = args[0];
+			lName = args[1];
+			
+			// Call methods to print the result
+			// Send both parameters for First and Last Name
+			// Args was set in Eclipse automatically.
+			printResult(fName, lName);
+		}
+		
+		
+		
+	}
+	
+	public static void printResult(String fName, String lName){
+		System.out.println("You first name is " + fName + " and your last name is " + lName);
+		System.out.println("You full name is " + fName + " " + lName);
 	}
 
 }
